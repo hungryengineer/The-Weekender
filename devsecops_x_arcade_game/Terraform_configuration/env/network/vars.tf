0254c8fa934680835a881f9dd651c2444e3d8e05
kanshum <kanshum@nagarro.com>
2024-03-16 13:45:51 +0530

    actions[wip]

--- devsecops_x_arcade_game/Terraform_configuration/env/network/vars.tf
+++ devsecops_x_arcade_game/Terraform_configuration/env/network/vars.tf
@@ -15,7 +15,7 @@ variable "destination_cidr_block" {
 }
 
 # #####for release based deployment/rollback####only for rollback branch
-variable "release_tag" {
-  description = "Tag or version of the release to which you want to roll back"
-  type        = string
-}
+# variable "release_tag" {
+#   description = "Tag or version of the release to which you want to roll back"
+#   type        = string
+# }
