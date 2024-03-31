bf4330c9c444ec2688a3f63764dd763c4430b904
kanshum <kanshum@nagarro.com>
2024-03-31 13:41:30 +0530

    df recreate

--- devsecops_x_arcade_game/Application/snake_game/main.py
+++ devsecops_x_arcade_game/Application/snake_game/main.py
@@ -1,37 +1,49 @@
-import random
 import time
 from turtle import Screen, Turtle
+from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
+
 myscreen = Screen()
 myscreen.setup(width=600, height=600)
 myscreen.bgcolor("black")
 myscreen.title("retro_snake_game")
 snake = Turtle()
-x_y_positions = [(0,0), (-10,0), (-20,0)]
-snakes_list = []
 myscreen.tracer(0) #smoothing out the animations
 
 '''below is the first module which handles snake objects creation and their starting position'''
-for positions in x_y_positions:
-    snake = Turtle()
-    snake.color("white")
-    snake.shape("square")
-    snake.penup()
-    snake.shapesize(0.5, 0.5, 0.5)
-    snake.goto(positions)
-    snakes_list.append(snake)
+snake_obj = Snake()
+'''below object creates the food for the snake'''
+food = Food()
+'''scoreboard'''
+score = ScoreBoard()
+
+'''to control the movement of the snake'''
+myscreen.listen()
+myscreen.onkey(snake_obj.up, "Up")
+myscreen.onkey(snake_obj.down, "Down")
+myscreen.onkey(snake_obj.left, "Left")
+myscreen.onkey(snake_obj.right, "Right")
 
 '''below is the second module which handles snake movements'''
 game_on = True
 while game_on:
     myscreen.update() #smoothing out the animation
-    time.sleep(0.3) #smooting out the animation
+    time.sleep(0.175) #smooting out the animation
+    snake_obj.snake_movement()
+    if snake_obj.head.distance(food) < 10: #food collision detection
+        food.refresh_food_location()
+        snake_obj.extend()
+        score.score_increament()
 
-    for snakes in range(len(snakes_list) - 1, 0, -1):
-        x_coordinates = snakes_list[snakes - 1].xcor()
-        y_coordinates = snakes_list[snakes - 1].ycor()
-        snakes_list[snakes].goto(x_coordinates, y_coordinates)
-    snakes_list[0].forward(10)  # Moved this line inside the loop
-    snakes_list[0].left(90)  # Moved this line inside the loop
+    if snake_obj.head.xcor() > 280 or snake_obj.head.xcor() < -280 or snake_obj.head.ycor() > 280 or snake_obj.head.ycor() < -280:
+        game_on = False
+        score.game_over()
+
+    for obj in snake_obj.snakes_list[1:]:
+        if snake_obj.head.distance(obj) < 10:
+            game_on = False
+            score.game_over()
 
 print(myscreen)
 myscreen.exitonclick()
