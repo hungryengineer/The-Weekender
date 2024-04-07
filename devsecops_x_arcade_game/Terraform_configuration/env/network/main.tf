19b021219eec81c8398be6881ba833e2f2cf98d3
kanshum <kanshum@nagarro.com>
2024-04-07 19:57:33 +0530

    decommissioning infra

--- devsecops_x_arcade_game/Terraform_configuration/env/network/main.tf
+++ devsecops_x_arcade_game/Terraform_configuration/env/network/main.tf
@@ -46,7 +46,7 @@ module "arcade-nat" {
     Name = "nat-${local.tags.Name}"
     Project = "nat-${local.tags.Project}"
     }
-    depends_on        = [ module.arcade-vpc, module.arcade-subnet ]
+    depends_on        = [ module.arcade-igw ]
         
 }
 
@@ -64,7 +64,7 @@ module "arcade-rt" {
     Name = "rt-${local.tags.Name}"
     Project = "rt-${local.tags.Project}"
     }
-    depends_on        = [ module.arcade-igw ]
+    depends_on        = [ module.arcade-igw ] 
 
 }
 
