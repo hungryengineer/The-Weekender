9da4882183c6b2bbb9034bf4333ca95626e25493
kanshum <kanshum@nagarro.com>
2024-05-12 09:38:38 +0530

    modified provider

--- devsecops_x_arcade_game/Terraform_configuration/env/kubernetes_namespace/main.tf
+++ devsecops_x_arcade_game/Terraform_configuration/env/kubernetes_namespace/main.tf
@@ -3,9 +3,13 @@ resource "null_resource" "update_config" {
   command = "aws eks --region ap-south-1 update-kubeconfig --name arcade-eks"
   }
 }
-
-module "kubernetes_namespace" {
-    source = "../../mod/kubernetes_namespace"
-    namespaces = var.namespaces
-    depends_on = [ null_resource.update_config ]
+resource "null_resource" "create_namespace" {
+  provisioner "local-exec" {
+  command = "kubectl create namespace argo"
+  }
 }
+# module "kubernetes_namespace" {
+#     source = "../../mod/kubernetes_namespace"
+#     namespaces = var.namespaces
+#     depends_on = [ null_resource.update_config ]
+# }
