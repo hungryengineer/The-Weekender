bcfdd74c23ddbcd4244357d1f0748395e803415c
kanshum <kanshum@nagarro.com>
2024-05-12 10:13:58 +0530

    datasources

--- devsecops_x_arcade_game/Terraform_configuration/env/kubernetes_namespace/main.tf
+++ devsecops_x_arcade_game/Terraform_configuration/env/kubernetes_namespace/main.tf
@@ -1,13 +1,15 @@
 resource "null_resource" "update_config" {
-  triggers = data.terraform_remote_state.arcade-k8s.outputs.eks_endpoint[0][0]
-
+  triggers = {
+    eks_ep = data.terraform_remote_state.arcade-k8s.outputs.eks_endpoint[0][0]
+  }
   provisioner "local-exec" {
     command = "aws eks --region ap-south-1 update-kubeconfig --name arcade-eks"
   }
 }
 resource "null_resource" "create_namespace" {
-  triggers = data.terraform_remote_state.arcade-k8s.outputs.eks_endpoint[0][0]
-
+  triggers = {
+    eks_ep = data.terraform_remote_state.arcade-k8s.outputs.eks_endpoint[0][0]
+  }
   provisioner "local-exec" {
     command = "kubectl create namespace argo"
   }
